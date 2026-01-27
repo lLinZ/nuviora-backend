0fb7616fef59089a91057e621768adcb80cc9b72
Jose Linares <linz.webdev@gmail.com>
2026-01-27 18:28:32 -0400

    feat: Create `order_change_extras` table to store additional details for order changes.

---
+++ database/migrations/2026_01_27_222000_create_order_change_extras_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_change_extras', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->unique()->constrained('orders')->onDelete('cascade');
+            $table->text('change_payment_details')->nullable();
+            $table->string('change_receipt')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_change_extras');
+    }
+};
